e4f034979e9d52a3c2e08c066254241306c40e36
yamamoto-makoto2020 <start.2020@icloud.com>
2020-11-12 21:56:54 +0900

    修正１

--- app/models/delivery_free.rb
+++ app/models/delivery_free.rb
@@ -4,6 +4,6 @@ class DeliveryFree < ActiveHash::Base
     { id: 2, name: '着払い(購入者負担)' },
     { id: 3, name: '送料込み(出品者負担)' }
   ]
-  # include ActiveHash::Associations
-  # has_many :items
+  include ActiveHash::Associations
+  has_many :items
 end
